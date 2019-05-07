706b9251cdef6b2663480c474fff00061092a184
LinhDinh <LinhDinh@DESKTOP-U36HJQS>
2019-05-07 11:12:57 +0700

    Add comment

--- IoC_Container_Demo/Resolver.cs
+++ IoC_Container_Demo/Resolver.cs
@@ -8,8 +8,11 @@ namespace IoC_Container_Demo
 {
 	public class Resolver
 	{
+		//Dictionary để chứa các interface và module tương ứng
 		private Dictionary<Type, Type> dependencyMap = new Dictionary<Type, Type>();
 
+		//Hai hàm cơ bản, ở đây mình chuyển <T> thành 
+		//dạng Type trong C# để dễ viết code
 		public T Resolve<T>()
 		{
 			return (T)Resolve(typeof(T));
@@ -31,10 +34,13 @@ namespace IoC_Container_Demo
 			{
 				throw new Exception(string.Format("Could not resolve type {0}", typeToResolve));
 			}
-
+			//Tìm constructor đầu tiên
 			var firstConstructor = resolveType.GetConstructors().First();
+			//Lấy các tham số của constructor
 			var constructorParameters = firstConstructor.GetParameters();
+			//Nếu như không có tham số
 			if (constructorParameters.Count() == 0)
+				//Khởi tạo module
 				return Activator.CreateInstance(resolveType);
 
 			IList<object> parameters = new List<object>();
@@ -42,6 +48,7 @@ namespace IoC_Container_Demo
 			{
 				parameters.Add(Resolve(parameterToResolve.ParameterType));
 			}
+			//Inject các dependency vào constructor của module
 			return firstConstructor.Invoke(parameters.ToArray());
 		}
 	}
